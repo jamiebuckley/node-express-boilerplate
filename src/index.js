567422a35107292dae4feb9da7aa5ed72531e4de
Jamie <jbuckley760@gmail.com>
2020-03-06 01:41:40 +0000

    add handlebars templating

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
+const path = require('path')
 const express = require('express')
 const logger = require('./logging')
 const app = express()
 const port = process.env.PORT || 8080
 
-/** Configure routing */
+/* Configure templating */
+const mustacheExpress = require('mustache-express')
+app.engine('html', mustacheExpress())
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
+app.use(express.static('public'))
+
+/* Configure routing */
 const router = express.Router()
 router.use('/v1', require('./api/v1'))
 app.use('/api', router)
-
 app.get('/', (req, res) => {
-  res.send('hi')
+  res.render('index')
 })
+
+/* Start application */
 app.listen(port, () => logger.info(`Application started on port ${port}`))
