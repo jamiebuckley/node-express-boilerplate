ce7d83762b59b259c950a906b89c9256bd5506b9
jamie <jbuckley760@gmail.com>
2020-03-06 02:53:28 +0000

    Add swagger docs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const express = require('express')
 const logger = require('./logging')
+const YAML = require('yamljs')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = YAML.load('./src/api/api.yml')
+
 const app = express()
 const port = process.env.PORT || 8080
 
@@ -8,8 +12,9 @@ const port = process.env.PORT || 8080
 const mustacheExpress = require('mustache-express')
 app.engine('html', mustacheExpress())
 app.set('view engine', 'html')
-app.set('views', __dirname + '/views')
+app.set('views', path.join(__dirname, '/views'))
 app.use(express.static('public'))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 /* Configure routing */
 const router = express.Router()
